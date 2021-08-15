9a0ed415fbbd01ed80d3149752a1a2c1ddf2c652
Akash Sharma <aakashsharma19090@gmail.com>
2021-08-15 15:02:24 +0530

    React refs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 class App extends Component {
+    
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            inputVal: ''
+        }
+
+        this.inputRef = React.createRef()
+    }
+    
+
+    setFocus = () => {
+
+        // ? Using 'Current' property to access Dom nodes using Refs
+        let inputElem = this.inputRef.current
+        inputElem.focus()
+        inputElem.classList.toggle('toggle')
+        this.setState( { inputVal: 'Toggled'  } )
+    }
+
     render() {
+
         return (
             <div>
-
+                <input ref={this.inputRef} type="text" value={this.state.inputVal} onChange={(e) => this.setState( { inputVal: e.target.value  } ) } />
+                <button onClick={this.setFocus} > Click Here </button>
             </div>    
+
         )
     }
 }
