14ab678cd6e15f7e470838f69f34f1556b9cf3e9
Akash Sharma <aakashsharma19090@gmail.com>
2021-08-15 08:11:16 +0530

    master commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
 import React, { Component } from 'react';
-import ThemeContext from './ThemeContext'
-import Theme from './Theme'
 class App extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            primaryTheme: 'theme1',
-            primaryThemeColor: '#496bbe',
-            secondaryTheme: 'theme2',
-            secondaryThemeColor: '#cccccc',
-            activetheme: 'Theme 1'
-        }
-    }
-
-    changeTheme = () => {
-        const currTheme = this.state.activetheme
-        if(currTheme === 'Theme 1' ){
-            this.setState({
-                ...this.state,
-                activetheme: 'Theme 2'
-            })
-        }else{
-            this.setState({
-                ...this.state,
-                activetheme: 'Theme 1'
-            })
-        }
-
-    }
-    
     render() {
         return (
-            <ThemeContext.Provider value={{
-                state: this.state,
-                changeTheme: this.changeTheme
-            }} >
-                <div className="App">
-                    <Theme/>
-                </div>
-            </ThemeContext.Provider>
-                
+            <div>
+
+            </div>    
         )
     }
 }
--- src/ThemeContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const themeContext = React.createContext(null)
-
-export default themeContext;
