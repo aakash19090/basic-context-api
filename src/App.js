3063216909fc52869e6c40193e48bf7a544e0ac2
Akash Sharma <aakashsharma19090@gmail.com>
2021-10-17 15:08:14 +0530

    simple  debouncing with uncontrollable input  -Akash

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React, { Component } from 'react';
-class App extends Component {
-    render() {
-        return (
-            <div>
+import React, { useState, useCallback } from 'react'
 
-            </div>    
-        )
+const App = () => {
+
+    const [inputValue2, setInputValue2] = useState('')
+
+    const callApi = async(event) => {
+        const fetchResponse = await fetch(`https://jsonplaceholder.typicode.com/users`)
+        const responseData = await fetchResponse.json()
+        console.log(responseData);
     }
+
+    const debouncedFunction = (func, delay) => {
+        let timer
+        return (...args) => {
+            const context = this
+            if(timer){
+                clearTimeout(timer)
+            }
+            timer = setTimeout(() => {
+                timer = null
+                func.apply(context, args)
+            }, delay)
+        }
+    }
+
+    const betterFunction = useCallback(debouncedFunction(callApi, 500), [])
+
+    return (
+        <div>
+            {/* Method 1 :- with Uncontrollable Input */}
+            <h3> Method 1 :- with Uncontrollable Input </h3>
+            <input type="text" onKeyUp={ betterFunction} />
+        </div>
+    )
 }
 
 export default App
