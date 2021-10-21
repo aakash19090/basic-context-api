79442fa7b24b0d1105f782b76b5db4340e0bae2f
Akash Sharma <aakashsharma19090@gmail.com>
2021-10-21 11:45:17 +0530

    simple  debouncing with uncontrollable input  -Akash

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import React, { useState, useCallback } from 'react'
+import debounce from 'lodash.debounce';
 
 const App = () => {
 
     const [inputValue2, setInputValue2] = useState('')
 
-    const callApi = async(event) => {
+    const callApi = debounce(async(event) => {
         const fetchResponse = await fetch(`https://jsonplaceholder.typicode.com/users`)
         const responseData = await fetchResponse.json()
         console.log(responseData);
-    }
+    }, 500) 
 
-    const debouncedFunction = (func, delay) => {
-        let timer
-        return (...args) => {
-            const context = this
-            if(timer){
-                clearTimeout(timer)
-            }
-            timer = setTimeout(() => {
-                timer = null
-                func.apply(context, args)
-            }, delay)
-        }
-    }
+    // const debouncedFunction = (func, delay) => {
+    //     let timer
+    //     return (...args) => {
+    //         const context = this
+    //         if(timer){
+    //             clearTimeout(timer)
+    //         }
+    //         timer = setTimeout(() => {
+    //             timer = null
+    //             func.apply(context, args)
+    //         }, delay)
+    //     }
+    // }
 
-    const betterFunction = useCallback(debouncedFunction(callApi, 500), [])
+    // const betterFunction = useCallback(debouncedFunction(callApi, 500), [])
 
     return (
         <div>
-            {/* Method 1 :- with Uncontrollable Input */}
-            <h3> Method 1 :- with Uncontrollable Input </h3>
-            <input type="text" onKeyUp={ betterFunction} />
+            <h3> Method 2 :- with Loadash </h3>
+            <input type="text" onKeyUp={(e) => callApi(e) } />
         </div>
     )
 }
